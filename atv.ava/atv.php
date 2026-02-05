bfefacc9e39eb5b9b16d12daed61030aed9d402a
duduu077 <165107812+duduu077@users.noreply.github.com>
2026-02-05 16:48:34 -0300

    Add files via upload

---
+++ atv.ava/atv.php
@@ -0,0 +1,18 @@
+<?php
+
+     class Produtos{
+
+
+        protected $nome;
+        protected $preco;
+        protected $quantidadeEstoque;
+
+        protected function __construct(string $nome, string $preco, string $quantidadeEstoque){
+            $this->nome = $nome;
+            $this->preco = $preco;
+            $this->quantidadeEstoque = $quantidadeEstoque;
+}
+ 
+echo 
+
+}
